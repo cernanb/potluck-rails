24a53f062ecc35d377ed0203d8e542d32d05b729
Cernan Bernardo <cernanb@gmail.com>
2017-11-09 23:47:57 -0800

    Build from_omniauth method in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.email = auth.info.email
+      user.uid = auth.uid
+      user.provider = auth.provider
+      user.image = auth.info.image
+      user.name = auth.info.name
+      user.facebook_oauth_token = auth.credentials.token
+      user.save!
+    end
+  end
 end
