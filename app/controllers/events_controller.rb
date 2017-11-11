9a696eb7ac556f5525865f3f070af76ce5ce8d1d
Cernan Bernardo <cernanb@gmail.com>
2017-11-10 21:49:12 -0800

    Add create and show action to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,29 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show]
   def new
     @event = Event.new
   end
+
+  def create
+    event = current_user.events.build(event_params)
+
+    if event.save
+      redirect_to event
+    else
+      redirect_to new_event_path
+    end
+  end
+
+  def show
+
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:text, :date, :street, :city, :state, :zip)
+    end
+
+    def set_event
+      @event = Event.find(params[:id])
+    end
 end
