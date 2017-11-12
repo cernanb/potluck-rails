2e440b1e13e8513535bc391f10235b8a5f1b4188
Cernan Bernardo <cernanb@gmail.com>
2017-11-11 20:32:41 -0800

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112041935) do
+ActiveRecord::Schema.define(version: 20171112042757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dishes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "event_dishes", force: :cascade do |t|
     t.boolean "claimed"
