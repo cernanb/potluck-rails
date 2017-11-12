755b5887ed2c55dc138f77e2b96594cea306b0cd
Cernan Bernardo <cernanb@gmail.com>
2017-11-11 20:53:24 -0800

    Add unclaimed_dishes method to Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,9 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :event_dishes
   has_many :dishes, through: :event_dishes
+
+  def unclaimed_dishes
+    event_dishes.where(claimed: false)
+  end
+
 end
