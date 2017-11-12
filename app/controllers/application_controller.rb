d8b77fdb5242b899196a3a45182bd812bd885b3c
Cernan Bernardo <cernanb@gmail.com>
2017-11-11 22:10:13 -0800

    Add logged_in helper method to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   def current_user
     session[:user_id].nil? ? nil : User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
   end
 end
