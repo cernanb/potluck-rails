c3f068a993bb40ce2082080169f85657b961786c
Cernan Bernardo <cernanb@gmail.com>
2017-11-11 23:45:23 -0800

    Build dishes controller with create action

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,2 +1,14 @@
 class DishesController < ApplicationController
+  def create
+    @event = Event.find(params[:event_id])
+    @dish = @event.dishes.build(dish_params)
+    if @event.save
+      redirect_to @event
+    end
+  end
+
+  private
+    def dish_params
+      params.require(:dish).permit(:name)
+    end
 end
