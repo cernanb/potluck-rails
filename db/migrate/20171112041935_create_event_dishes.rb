940da709613f92f5867180ce11c342b3d75f295c
Cernan Bernardo <cernanb@gmail.com>
2017-11-11 20:42:48 -0800

    Add default for event_dishes table

--- db/migrate/20171112041935_create_event_dishes.rb
+++ db/migrate/20171112041935_create_event_dishes.rb
@@ -1,7 +1,7 @@
 class CreateEventDishes < ActiveRecord::Migration[5.1]
   def change
     create_table :event_dishes do |t|
-      t.boolean :claimed
+      t.boolean :claimed, default: false
       t.integer :dish_id
       t.integer :event_id
       t.integer :user_id
