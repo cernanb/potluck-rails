078fa349e18c9c652219635e4266200433f60e0f
Cernan Bernardo <cernanb@gmail.com>
2017-11-11 21:54:11 -0800

    Add update action to EventDishesController

--- app/controllers/event_dishes_controller.rb
+++ app/controllers/event_dishes_controller.rb
@@ -1,2 +1,13 @@
 class EventDishesController < ApplicationController
+  before_action :set_event_dish, only: [:update]
+
+  def update
+    @event_dish.update(claimed: true, user: current_user)
+    redirect_to @event_dish.event
+  end
+
+  private
+    def set_event_dish
+      @event_dish = EventDish.find(params[:id])
+    end
 end
