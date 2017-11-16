e0451f68276a9eb20b25453997c3fbec45cb6924
Cernan Bernardo <cernanb@gmail.com>
2017-11-15 21:59:04 -0800

    Update sessions to include user sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,25 @@
 class SessionsController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
-    user = User.from_omniauth(request.env["omniauth.auth"])
+    if request.env["omniauth.auth"]
+      user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if user.valid?
-      session[:user_id] = user.id
-      redirect_to request.env['omniauth.origin']
+      if user.valid?
+        session[:user_id] = user.id
+        redirect_to events_path
+      end
+    else
+      user = User.find_by(email: params[:user][:email])
+      
+      if user && user.authenticate(params[:user][:password])
+        session[:user_id] = user.id
+        redirect_to events_path
+      else
+        redirect_to login_path
+      end
     end
   end
 
