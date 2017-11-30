39d2a947f515edfeed9ae0e6f76ad46ee9cd2bf4
Cernan Bernardo <cernanb@gmail.com>
2017-11-29 21:12:42 -0800

    Add admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116051742) do
+ActiveRecord::Schema.define(version: 20171129064208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20171116051742) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
