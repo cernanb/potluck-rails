ee8807805fd8a13c0c635c1fbcdb6cb11d564bbb
Cernan Bernardo <cernanb@gmail.com>
2017-11-30 22:35:28 -0800

    Add association between events and comments

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :event_dishes
   has_many :dishes, through: :event_dishes
+  has_many :comments
 
   def unclaimed_dishes
     event_dishes.where(claimed: false)
