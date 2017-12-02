987f6959434539ec4ed44619bf2918667239affa
Cernan Bernardo <cernanb@gmail.com>
2017-12-01 16:16:02 -0800

    Add interval to ping heroku app every 5 minutes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,12 @@
-import UIkit from 'uikit'
-import Icons from 'uikit/dist/js/uikit-icons'
+import UIkit from "uikit"
+import Icons from "uikit/dist/js/uikit-icons"
+import http from "http"
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // loads the Icon plugin
   UIkit.use(Icons)
+
+  setInterval(function() {
+    http.get("http://bernardo-potluck.herokuapp.com/")
+  }, 30000)
 })
